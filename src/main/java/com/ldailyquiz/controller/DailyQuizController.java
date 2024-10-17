a2cb496b5c2bec8d39e72fa3dd94d4e84a97a662
DevHaeng <dev.haeng@gmail.com>
2024-10-17 12:29:38 +0900

    - java 기능 세분화

--- src/main/java/com/ldailyquiz/controller/DailyQuizController.java
+++ src/main/java/com/ldailyquiz/controller/DailyQuizController.java
@@ -1,5 +1,8 @@
 package com.ldailyquiz.controller;
 
+import com.ldailyquiz.dto.GuessRequestDto;
+import com.ldailyquiz.service.DailyQuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,63 +14,20 @@ import java.util.Map;
 
 @RestController
 public class DailyQuizController {
-
-    private String hint = "이 단어는 과일의 이름입니다.";
+    @Autowired
+    private DailyQuizService service;
 
     @PostMapping("/check-guess")
-    public ResponseEntity<Map<String, Object>> checkGuess(@RequestBody GuessRequest request) {
-        String guess = request.getGuess();
-        String answer = "APPLE"; // 실제 게임에서는 동적으로 설정
-        String[] result = new String[5];
-        boolean isCorrect = true;
-
-        for (int i = 0; i < 5; i++) {
-            if (guess.charAt(i) == answer.charAt(i)) {
-                result[i] = "correct";
-            } else if (answer.indexOf(guess.charAt(i)) != -1) {
-                result[i] = "present";
-                isCorrect = false;
-            } else {
-                result[i] = "absent";
-                isCorrect = false;
-            }
-        }
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("result", result);
-        response.put("isCorrect", isCorrect);
-        response.put("attemptNumber", request.getAttemptNumber());
-
+    public ResponseEntity<Map<String, Object>> checkGuess(@RequestBody GuessRequestDto request) {
+        Map<String, Object> response = service.checkGuess(request.getGuess(), request.getAttemptNumber());
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/get-hint")
     public ResponseEntity<Map<String, String>> getHint() {
+        String hint = service.getHint();
         Map<String, String> response = new HashMap<>();
         response.put("hint", hint);
         return ResponseEntity.ok(response);
-    }
-
-    // GuessRequest 클래스 정의
-    static class GuessRequest {
-        private String guess;
-        private int attemptNumber;
-
-        // Getters and setters
-        public String getGuess() {
-            return guess;
-        }
-
-        public void setGuess(String guess) {
-            this.guess = guess;
-        }
-
-        public int getAttemptNumber() {
-            return attemptNumber;
-        }
-
-        public void setAttemptNumber(int attemptNumber) {
-            this.attemptNumber = attemptNumber;
-        }
     }
 }
---
+++ src/main/java/com/ldailyquiz/entity/DailyQuiz.java
@@ -0,0 +1,22 @@
+package com.ldailyquiz.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Table(name = "DAILY_QUIZ")
+@Entity
+public class DailyQuiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DAILY_QUIZ_SEQ")
+    @SequenceGenerator(name = "DAILY_QUIZ_SEQ", sequenceName = "DAILY_QUIZ_SEQ", allocationSize = 1)
+    private Long id;
+
+    @Column(nullable = false)
+    private String answer;
+
+    @Column(nullable = false)
+    private String hint;
+}
